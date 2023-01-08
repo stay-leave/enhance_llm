00fdfdd21ce3d527ad8476343ca6518292fba907
yitacls <75364857+yitacls@users.noreply.github.com>
2023-01-08 14:27:12 +0800

    weibo_blog.py
    1.报错：UnboundLocalError: local variable 'XXX' referenced before assignment
    解决：部分代码没有对齐，如第159~162行，导致循环外使用循环内定义变量，所以需要增加缩进
    2.文件保存前需要在weibo-crawler-main目录下创建'微博正文'文件夹
    3.soup.select函数路径与我使用的微博网页不匹配，导致爬取内容缺失，所以对相关代码进行了修改

--- weibo_blog.py
+++ weibo_blog.py
@@ -109,7 +109,7 @@ def get_page(url,headers):
             print("Connection refused by the server..")
             print("Let me sleep for 5 seconds")
             print("ZZzzzz...")
-            time.sleep(5)
+            time.sleep(10)
             print("Was a nice sleep, now let me continue...")
             continue
     html=resp.text#网页
@@ -172,107 +172,172 @@ def get_blogs(url,headers):
     comment_data=[]
     like_data=[]
 
+#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div > div.card-act > ul > li:nth-child(4) > a
+#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div > div.card-act > ul > li:nth-child(4) > a > em
+#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(4) > a:nth-child(1) > button:nth-child(1) > span:nth-child(2)
+
     #获取单页博文的详细信息
     for i in range(1, blogs_number+1):#如果是第一个，就保留第一个的选择器，如果第2及之后，就换
         if i==1:
             blog_ids=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1)')#数字id
             user_infos=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)')
-            publish_times=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > a:nth-child(1)')
-            blog_texts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > p:nth-child(3)')
-            reposts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)')
-            comments=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)')
-            like_counts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1) > button:nth-child(1) > span:nth-child(2)')
+            #publish_times = soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > a:nth-child(1)')
+            publish_times=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div > div.card-feed > div.content > p.from > a:nth-child(1)')
+            #blog_texts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > p:nth-child(3)')
+            blog_texts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > p:nth-child(2)')
+            #reposts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)')
+            reposts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)')
+            #comments=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)')
+            comments = soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1)')
+            #like_counts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(4) > a:nth-child(1) > button:nth-child(1) > span:nth-child(2)')
+            like_counts=soup.select('#pl_feedlist_index > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(4) > a:nth-child(1) > em')
         else: 
             blog_ids=soup.select('div.card-wrap:nth-child('+str(i)+')')
             user_infos=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)')
-            publish_times=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > a:nth-child(1)')
-            blog_texts=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > p:nth-child(3)')
-            reposts=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)')
-            comments=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)')
-            like_counts=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1) > button:nth-child(1) > span:nth-child(2)')
-            
+            #publish_times=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > a:nth-child(1)')
+            publish_times = soup.select('div.card-wrap:nth-child('+str(i)+') > div > div.card-feed > div.content > p.from > a:nth-child(1)')
+            #blog_texts=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > p:nth-child(3)')
+            blog_texts=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > p:nth-child(2)')
+            #reposts=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)')
+            reposts=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)')
+            #comments=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1)')
+            comments=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1)')
+            #like_counts=soup.select('div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1) > button:nth-child(1) > span:nth-child(2)')
+            like_counts=soup.select('div.card-wrap:nth-child('+str(i)+') > div.card > div.card-act > ul > li:nth-child(4) > a > em')
+
+#pl_feedlist_index > div:nth-child(1) > div:nth-child(4) > div.card > div.card-act > ul > li:nth-child(4) > a > em
+
             #div.card-wrap:nth-child('+str(i)+') > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)
         #博文id、bid提取
+
         for b in blog_ids:
             blog_id=b#bs4.element.Tag类型
-        blog_id=str(blog_id)#str类型
-        blog_id=re.findall('<div action-type="feed_list_item" class="card-wrap" mid="(.*?)">',blog_id,re.S)#提取博文id，如['4809724744696284']
-        blog_id=[int(id) for id in blog_id]
-        blog_bid=[str(mid_to_url(id)) for id in blog_id]
-        id_data += blog_id
-        bid_data += blog_bid
+            blog_id=str(blog_id)#str类型
+            blog_id=re.findall('<div action-type="feed_list_item" class="card-wrap" mid="(.*?)">',blog_id,re.S)#提取博文id，如['4809724744696284']
+            blog_id=[int(id) for id in blog_id]
+            blog_bid=[str(mid_to_url(id)) for id in blog_id]
+            id_data += blog_id
+            bid_data += blog_bid
 
         #用户信息提取
         for u in user_infos:
             user_info=u#bs4.element.Tag类型
-        user_info=str(user_info)#str类型
-        uids=re.findall('<a class="name" href="//weibo.com/(.*?)?refer_flag=.*?',user_info)#析取出uid
-        uid=[uid.replace('?','') for uid in uids]#如：['1888116862']
-        name=re.findall('<a class=.*?nick-name="(.*?)" suda-data=.*?',user_info)#析取出name，如：['投鱼问道']
-        uid_data += uid
-        name_data += name
+            user_info=str(user_info)#str类型
+            uids=re.findall('<a class="name" href="//weibo.com/(.*?)?refer_flag=.*?',user_info)#析取出uid
+            uid=[uid.replace('?','') for uid in uids]#如：['1888116862']
+            name=re.findall('<a class=.*?nick-name="(.*?)" suda-data=.*?',user_info)#析取出name，如：['投鱼问道']
+            uid_data += uid
+            name_data += name
+            #print(uid)
 
         #发布时间提取
+        # for p in publish_times:
+        #     publish_time=p#bs4.element.Tag类型
+        # publish_time=str(publish_time)#str 
+        # publish_time=re.findall('<a href="//weibo.com.*?>(.*?)</a>',publish_time,re.S)#提取时间，如['09月03日 21:20']
+        # publish_time=[publish.strip() for publish in publish_time]#去除前后空白
+        # time_data += publish_time
         for p in publish_times:
             publish_time=p#bs4.element.Tag类型
-        publish_time=str(publish_time)#str 
-        publish_time=re.findall('<a href="//weibo.com.*?>(.*?)</a>',publish_time,re.S)#提取时间，如['09月03日 21:20']
-        publish_time=[publish.strip() for publish in publish_time]#去除前后空白
-        time_data += publish_time
+            publish_time=str(publish_time)#str 
+            publish_time=re.findall('<a href="//weibo.com.*?>(.*?)</a>',publish_time,re.S)#提取时间，如['09月03日 21:20']
+            publish_time=[publish.strip() for publish in publish_time]#去除前后空白
+            time_data += publish_time
 
         #博文文本提取
+        # for b in blog_texts:
+        #     blog_text=b#bs4.element.Tag类型
+        # blog_text=str(blog_text)#str 
+        # blog_text=re.findall('<p class="txt".*?>(.*?)</p>',blog_text,re.S)#提取文本
+        # blog_text=[text.strip().strip('\u200b').replace(' ','') for text in blog_text]#去空格、非法字符
+        # text_data += blog_text
         for b in blog_texts:
             blog_text=b#bs4.element.Tag类型
-        blog_text=str(blog_text)#str 
-        blog_text=re.findall('<p class="txt".*?>(.*?)</p>',blog_text,re.S)#提取文本
-        blog_text=[text.strip().strip('\u200b').replace(' ','') for text in blog_text]#去空格、非法字符
-        text_data += blog_text
+            blog_text=str(blog_text)#str 
+            blog_text=re.findall('<p class="txt".*?>(.*?)</p>',blog_text,re.S)#提取文本
+            blog_text=[text.strip().strip('\u200b').replace(' ','') for text in blog_text]#去空格、非法字符
+            text_data += blog_text
 
+        
         #转发数提取
         for r in reposts:
             repost=r#bs4.element.Tag类型
-        repost=str(repost)#str 
-        repost=re.findall('<a.*?</span> (.*?)</a>',repost,re.S)#提取转发数
-        new_repost=[]
-        for r in repost:#处理无转发的情况，转发数转为int
-            if r.strip() == '转发':
-                r=0
-            else:
-                r=int(r)
-            new_repost.append(r)
-        repost_data += new_repost
+            repost=str(repost)#str
+            #repost=re.findall('<a.*?</span> (.*?)</a>',repost,re.S)#提取转发数
+            repost = re.findall('<a.*?>(.*?)</a>',repost, re.S)  # 提取转发数
+            new_repost=[]
+            for r in repost:#处理无转发的情况，转发数转为int
+                if r.strip() == '转发':
+                    r=0
+                else:
+                    #r=int(r)
+                    r = re.findall('[0-9]+', r, re.S)  # 提取评论数
+                    #print(r)
+                    r=int(r[0])
+                new_repost.append(r)
+            repost_data += new_repost
 
         #评论数提取
+        #print(str(comments))
         for c in comments:
             comment=c#bs4.element.Tag类型
-        comment=str(comment)#str 
-        comment=re.findall('<a.*?</span> (.*?)</a>',comment,re.S)#提取评论数
-        new_comment=[]
-        for c in comment:#处理无评论的情况，评论数转为int
-            if c.strip() == '评论':
-                c=0
-            else:
-                c=int(c)
-            new_comment.append(c)
-        comment_data += new_comment
+            comment=str(comment)#str 
+            #print("comment:"+comment)
+            #comment=re.findall('<a.*?</span> (.*?)</a>',comment,re.S)#提取评论数
+            comment=re.findall('<a.*?>(.*?)</a>',comment,re.S)#提取评论数
+            #print(comment)
+            new_comment=[]
+            for c in comment:#处理无评论的情况，评论数转为int
+                if c.strip() == '评论':
+                    c=0
+                else:
+                    #int(c)
+                    c = re.findall('[0-9]+', c, re.S)  # 提取评论数
+                    #print(c)
+                    c=int(c[0])
+                new_comment.append(c)
+            comment_data += new_comment
 
         #点赞数提取
+        # for l in like_counts:
+        #     like_count=l#bs4.element.Tag类型
+        # like_count=str(like_count)#str 
+        # #like_count=re.findall(r'\b\d+\b',like_count,re.S)#匹配纯数字
+        # like_count_1=re.findall('<span class="woo-like-count">(.*?)</span>',like_count,re.S)#匹配点赞内容
+        # if like_count_1 == []:#处理第一个博文的点赞规则和之后不一样的问题
+        #     like_count_1=re.findall('</span> (.*?)</a>',like_count,re.S)
+        # new_like_count=[]
+        # for l in like_count_1:#处理无点赞的情况，点赞数转为int
+        #     if l.strip() == '赞':
+        #         l=0
+        #     else:
+        #         l=int(l)
+        #     new_like_count.append(l)
+        # like_data += new_like_count
+        # time.sleep(1)
+        #print(str(like_counts))
         for l in like_counts:
             like_count=l#bs4.element.Tag类型
-        like_count=str(like_count)#str 
-        #like_count=re.findall(r'\b\d+\b',like_count,re.S)#匹配纯数字
-        like_count_1=re.findall('<span class="woo-like-count">(.*?)</span>',like_count,re.S)#匹配点赞内容
-        if like_count_1 == []:#处理第一个博文的点赞规则和之后不一样的问题
-            like_count_1=re.findall('</span> (.*?)</a>',like_count,re.S)
-        new_like_count=[]
-        for l in like_count_1:#处理无点赞的情况，点赞数转为int
-            if l.strip() == '赞':
-                l=0
-            else:
-                l=int(l)
-            new_like_count.append(l)
-        like_data += new_like_count
-        time.sleep(1)
+            like_count=str(like_count)#str 
+            # print(str(like_count))
+            # print("====")
+            #like_count=re.findall(r'\b\d+\b',like_count,re.S)#匹配纯数字
+            like_count_1=re.findall('<em>(.*?)</em>',like_count,re.S)#匹配点赞内容
+            # print(like_count_1)
+            # print("====like_count_1====")
+            # if like_count_1 == []:#处理第一个博文的点赞规则和之后不一样的问题
+            #     like_count_1=re.findall('</span> (.*?)</a>',like_count,re.S)
+            new_like_count=[]
+            for l in like_count_1:#处理无点赞的情况，点赞数转为int
+                if l.strip() == '':
+                    l=0
+                else:
+                    l = re.findall('[0-9]+', l, re.S)  # 提取评论数
+                    # print(l)
+                    l=int(l[0])
+                new_like_count.append(l)
+            like_data += new_like_count
+            time.sleep(1)
     
     return id_data,bid_data,uid_data,name_data,time_data,text_data,repost_data,comment_data,like_data#返回单页的数据
 
@@ -291,6 +356,7 @@ if __name__ == '__main__':
 'Accept-Encoding':'gzip, deflate, br',
 'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
 'Connection':'keep-alive',
+
 'Cookie':'',#填入自己的
 'Host':'s.weibo.com',
 'Referer':'https://weibo.com/',
@@ -309,10 +375,10 @@ if __name__ == '__main__':
     parameters_two=['&suball=1','&haspic=1','&hasvideo=1','&hasmusic=1','&haslink=1',]#二级条件全选，含图片，含视频，含音乐，含短链
 
     #需要自定义
-    one_type=1#微博类型，一级条件筛选。0为全选，1为热门，2为原创，3为认证用户，4为媒体，5为观点
+    one_type=0#微博类型，一级条件筛选。0为全选，1为热门，2为原创，3为认证用户，4为媒体，5为观点
     two_type=0#细分类型，二级条件筛选。0为全选，1为含图片，2为含视频，3为含音乐，4为含短链接
-    timescope='2022-09-02-0:2022-09-06-23'#开始时间：结束时间，年-月-日-时，其中时是从0到23
-    query='中国'#关键词或话题
+    timescope='2022-12-21-0:2023-01-07-0'#开始时间：结束时间，年-月-日-时，其中时是从0到23
+    query='2023新年贺词'#关键词或话题
     #query=parse.quote(query)#将搜索关键词转换编码
 
     #存储单次搜索的所有数据
